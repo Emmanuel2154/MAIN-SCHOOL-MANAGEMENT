aeb073d6b6810ffbc81fcfc6db4fc39a86f05274
Emmanuel <emmanuelnwokoma364@gmail.com>
2025-08-29 19:26:19 +0100

    Updated get Student Logic

--- main.py
+++ main.py
@@ -1,22 +1,39 @@
-from services.student_services import add_student_to_db
-from services.employee_services import add_employee_to_db, create_new_employee
+from services.student_services import create_new_student
+# from sql_alchemy import add_student_to_database
+# from services.employee_services import add_employee_to_db, create_new_employee
+from storage.database import DBstorage
 from models.student import Student
-from utils.fiile_handler import append_file
-from data import student as student_obj
-from data import employee as employee_obj
-from pprint import pprint
-def main():    
+from models.employee import Employee
+from models.user import User
+import asyncio
+
+from uuid import uuid4
+# from utils.fiile_handler import append_file
+from data import student as student_dict
+# from data import employee as employee_obj
+# from pprint import pprint
+
+
+db = DBstorage()
+
+async def main(): 
+       
     try:
-        # student = add_student_to_db(student_obj)
-        student = Student(**student_obj)
-        # print(student)
-        student.to_dict()
-        student.save_to_json()
+    
+        # add_student_to_database(student_dict)
 
-
-
-        # employee = add_employee_to_db(employee_obj)
+        # db = DBstorage()
+        # async for session in db.get_session():
+        #     new_user = User(id=str(uuid4()), email="anything", password="anything")
+        #     db.add(session, new_user)
+        #     await session.commit()
+        # await db.drop_tables()
+        # await db.create_table()
+        # result = await create_new_student(student_dict)
+        # print(result)
+        async for session in db.get_session():
+            
     except ValueError as e:
         print(e)
-
-main()
+    
+asyncio.run(main())
